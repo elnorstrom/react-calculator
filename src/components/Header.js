9335f772719ebdcdfd9ef87547a73dbfe4e4a781
Home <elias.norstromm@gmail.com>
2018-07-19 19:34:19 +0100

    component files separation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = () => (
-    <div>
-      <h1>Kalculatör</h1>
-      <p>Numbers...numbers...</p>
-    </div>
-  );
+  <div>
+    <h1>Kalculatör</h1>
+    <p>Numbers...numbers...</p>
+  </div>
+);
 
 export default Header;
